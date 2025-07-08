38cb71507407165ab940ea27b35d9d94f69ed2c7
Harsh Dhiman <153075841+dhimanharsh@users.noreply.github.com>
2025-07-08 16:47:27 +0530

    Update Main.js button removed

--- source/Components/Main.js
+++ source/Components/Main.js
@@ -25,7 +25,7 @@ export default function Main() {
 <section>
     <h1>Discover Unmatched Quality and Style</h1>
     <p>Where every click opens the door to a world of exceptional products designed just for you.At the heart of every exceptional product lies an unwavering commitment to quality. When you choose to discover unmatched quality, you are not just making a purchase; you are investing in craftsmanship that stands the test of time. Our products are meticulously crafted using the finest materials, ensuring durability and elegance in every detail. Experience the satisfaction of owning items that not only look stunning but also perform flawlessly, enhancing your lifestyle in ways you never imagined</p>
-    <button>Shop Now</button>
+
 </section>
 
 
